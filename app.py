4698fac201aeea3a3dee5a19f948c02c0fa6c81e
Rafal Puszkarewicz <puszkarewicz.rafal@gmail.com>
2021-01-04 18:00:30 +0100

    clean up

--- app.py
+++ app.py
@@ -1,28 +1,23 @@
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
 app.config["FREEZER_DESTINATION"] = "docs"
 
 
 @app.route("/")
-@app.route("/index.html")
 def index():
     return render_template("index.html")
 
 
-@app.route("/about.html")
+@app.route("/about/")
 def about():
     return render_template("about.html")
 
 
-@app.route("/services.html")
-def services():
-    return render_template("services.html")
-
-
-@app.route("/bus.html")
-def bus():
-    return render_template("bus.html")
+@app.route("/contact/")
+def contact():
+    return render_template("contact.html")
 
 
 if __name__ == "__main__":
